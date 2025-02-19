3bbbe2e6e5558dfa8978741ddb1e7b42746d88ea
ARADJO CREPIN <aradjocrepin@gmail.com>
2025-02-19 10:44:52 +0000

    Ajout de la route vers urls local

--- gestion/boutique/views.py
+++ gestion/boutique/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect,get_object_or_404
+from django.db.models import Count, Sum
+from django.db.models.functions import TruncMonth
+from datetime import datetime, timedelta
+from .models import Categorie,Produit,Commande,Facture,LigneCommande
+from django.contrib import messages
+from .forms import ProduitForm,CategorieForm,ClientForm
+# Create your views here.
+def index(request):
+    categories = Categorie.objects.all()
+    return render(request,'new_boutique/index.html',{'categories':categories})
