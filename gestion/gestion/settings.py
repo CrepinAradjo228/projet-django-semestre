4ae5a8e8194877affbb4045ff814b29dba3923db
ARADJO CREPIN <aradjocrepin@gmail.com>
2025-02-19 09:54:22 +0000

    Modification du fichier settings pour inclure ma base de donnée

--- gestion/gestion/settings.py
+++ gestion/gestion/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'gestion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'boutique',
+        'USER' : 'postgres',
+        'PASSWORD': 'Cabricit2005',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
+
 }
 
 
